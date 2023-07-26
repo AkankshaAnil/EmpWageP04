a86c526b792069c460b8bbd7bb6ad180c63f2def
AkankshaAnil <akankshakodgirwar07@gmail.com>
2023-07-26 16:03:10 +0530

    [AkankshaAnil] Checking for employee daily wage

--- EmpWageP04/attendence.cs
+++ EmpWageP04/attendence.cs
@@ -10,19 +10,23 @@ namespace EmpWageP04
     {
         public  void FindAttendence()
         {
-            Console.WriteLine(" Welcome to Employment Wage Problem 101");
+            Console.WriteLine("Welcome to the Employee Wage Calculator!");
 
             Random random = new Random();
-            bool isPresent = random.Next(2) == 0;
+            bool isPresent = random.Next(0, 2) == 0;
 
             if (isPresent)
             {
+                int fullTimeWorkHours = 8;
+                int hourlyPayRate = 20;
+                int dailyWage = fullTimeWorkHours * hourlyPayRate;
                 Console.WriteLine("Employee is present.");
+                Console.WriteLine("Daily wage: $" + dailyWage);
             }
             else
             {
                 Console.WriteLine("Employee is absent.");
-
+                Console.WriteLine("No daily wage earned.");
             }
         }
     }
